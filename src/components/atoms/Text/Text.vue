c4e864dfbae1459ed3fe12fda7ffb23fd7926758
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-06 21:20:47 +0100

    Add text

---
+++ src/components/atoms/Text/Text.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    {{text}}
+  </span>  
+</template>
+
+<script>
+export default {
+  name: "v-text",
+  props: ["text"]
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 16pt;
+}
+</style>
