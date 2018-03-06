49526ef61002722a5681a77b314b216b9da6919d
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-06 21:20:29 +0100

    Add Container

---
+++ src/components/atoms/Container/Container.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <md-card class="md-primary container">
+      <div>
+        <slot name="header"></slot>
+      </div>
+      <div>
+        <slot></slot>
+      </div>
+      <div>
+        <slot name="footer"></slot>
+      </div>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-container"
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  height: 200px;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+}
+</style>
