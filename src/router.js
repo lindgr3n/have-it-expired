078b21b9b685c9e3b85e415c76ac50d1367b93d8
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-06 21:19:45 +0100

    Add routing

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
 
 Vue.use(Router);
 
@@ -16,6 +18,16 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
     }
   ]
 });
