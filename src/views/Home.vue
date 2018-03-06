b6da288fae6d421ad438d183b5fa40e4076db71f
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-06 21:21:26 +0100

    Refactor home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <v-container />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TimeToExpireCard from "@/components/molecules/TimeToExpireCard";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    "v-container": TimeToExpireCard
+  },
+
+  data() {
+    return {
+      items: {}
+    };
   }
 };
 </script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
