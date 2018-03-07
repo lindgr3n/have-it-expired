a5e27ba372b22aa2ed74973fe9041263bf237330
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-07 08:48:36 +0100

    Added login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    Login
+  <div class="content">
+    <v-login-dialog />
   </div>
 </template>
 
 <script>
+import LoginDialog from "@/components/organism/LoginDialog";
 export default {
-  name: "login"
+  name: "login",
+
+  components: {
+    "v-login-dialog": LoginDialog
+  }
 };
 </script>
 
 <style scoped>
+.content {
+  display: flex;
+  justify-content: center;
+}
 </style>
