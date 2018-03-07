cd52bda1b4e3f2f7583c70c18968987c20f204d3
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-07 08:47:33 +0100

    LoginDialog

---
+++ src/components/organism/LoginDialog/LoginDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dialog">
+    <v-login />
+  </div>
+</template>
+
+<script>
+import Login from "@/components/molecules/Login";
+export default {
+  name: "login-dialog",
+
+  components: {
+    "v-login": Login
+  }
+};
+</script>
+
+<style scoped>
+.dialog {
+  width: 300px;
+}
+</style>
