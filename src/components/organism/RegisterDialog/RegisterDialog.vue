2246cbb0e51f03e1d123957112af03ef5ea37225
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-07 19:10:10 +0100

    Register component

---
+++ src/components/organism/RegisterDialog/RegisterDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dialog">
+    <v-register />
+  </div>
+</template>
+
+<script>
+import firebase from "@/firebase";
+
+import Register from "@/components/molecules/Register";
+export default {
+  name: "v-register-dialog",
+
+  components: {
+    "v-register": Register
+  },
+
+  mounted() {
+    console.log(firebase);
+  }
+};
+</script>
+
+<style scoped>
+.dialog {
+  width: 300px;
+}
+</style>
