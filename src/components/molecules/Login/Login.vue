b26f482e98df5c31ed75a33788eace5958471b71
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-07 08:46:52 +0100

    Add Login

---
+++ src/components/molecules/Login/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <md-card class="login-content">
+    <md-field md-clearable>
+      <label>Email</label>
+      <md-input v-model="initial"></md-input>
+    </md-field>
+
+    <md-field>
+      <label>Password</label>
+      <md-input v-model="password" type="password"></md-input>
+    </md-field>
+    <p>Register here <router-link to="/register">Register</router-link></p>
+    <md-button class="md-raised md-primary" v-on:click="login">Login</md-button>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: "login",
+
+  methods: {
+    login() {
+      console.log("Wohooo");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-content {
+  padding: 20px;
+}
+</style>
