820bde613a7ba817713847dd6f7cfbc97a358c0b
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-07 21:59:55 +0100

    Make sure firebase is auth

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import firebase, { onAuthenticationChanged } from "./firebase";
 
 Vue.config.productionTip = false;
 
@@ -16,5 +17,34 @@ Vue.use(VueMaterial);
 new Vue({
   router,
   store,
+  data: {
+    firebase
+  },
+  created() {
+    onAuthenticationChanged().then(user => {
+      console.log("authChanged", user);
+      if (user) {
+        this.$router.push("/home");
+      } else {
+        this.$router.push("/login");
+      }
+    });
+  },
+
   render: h => h(App)
 }).$mount("#app");
+
+// onInitialized().then(() => {
+//   console.log("test", app);
+//   if (!app) {
+//     app = new Vue({
+//       router,
+//       store,
+//       data: {
+//         firebase
+//       },
+//       render: h => h(App)
+//     }).$mount("#app");
+//   }
+//   console.log(app);
+// });
