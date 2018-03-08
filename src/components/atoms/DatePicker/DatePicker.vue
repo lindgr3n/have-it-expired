bbf62d8bd12853ed3b34cce8d705bd991c816f56
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-09 00:31:25 +0100

    Added datepicker

---
+++ src/components/atoms/DatePicker/DatePicker.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-menu
+      ref="menu"
+      lazy
+      :close-on-content-click="false"
+      v-model="menu"
+      transition="scale-transition"
+      offset-y
+      full-width
+      :nudge-right="40"
+      min-width="290px"
+      :return-value.sync="date"
+    >
+      <v-text-field
+        slot="activator"
+        label="Picker in menu"
+        v-model="date"
+        prepend-icon="event"
+        readonly
+      ></v-text-field>
+      <v-date-picker v-model="date" no-title scrollable>
+        <v-spacer></v-spacer>
+        <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+        <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+      </v-date-picker>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-date",
+
+  data() {
+    return {
+      menu: false,
+      modal: false,
+      date: ""
+    };
+  },
+
+  watch: {
+    date(value) {
+      this.$emit("boughtChange", value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
