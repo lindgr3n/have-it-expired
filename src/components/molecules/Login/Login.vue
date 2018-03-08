f45decea9a914a98784b512ed085bca910818b8c
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-08 21:12:32 +0100

    Show error

--- src/components/molecules/Login/Login.vue
+++ src/components/molecules/Login/Login.vue
@@ -7,6 +7,12 @@
     </v-text-field>
     <p>Register here <router-link to="/register">Register</router-link></p>
     <v-btn color="info" v-on:click="submit">Login</v-btn>
+    <v-layout row v-if="error">
+      <v-snackbar :value="showSnackbar" color="error" top>
+        {{ error }}
+      </v-snackbar>
+    </v-layout>
+   
   </v-card>
 </template>
 
@@ -14,13 +20,20 @@
 export default {
   name: "login",
 
-  props: ["onLogin"],
+  props: ["onLogin", "loading", "error"],
+
   data() {
     return {
       email: "",
       password: ""
     };
   },
+  computed: {
+    showSnackbar() {
+      return this.error !== null && this.error !== undefined;
+    }
+  },
+
   methods: {
     submit() {
       this.onLogin({ email: this.email, password: this.password });
