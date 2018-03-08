100c51f8db5b7b99a4102b6307819669409a9f2e
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-09 00:32:52 +0100

    Register dialog

---
+++ src/components/organism/RegisterDialog/RegisterDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-register :onSubmit="onSubmit" />
+  </div>
+</template>
+
+<script>
+import Register from "@/components/molecules/Register";
+export default {
+  name: "register-dialog",
+
+  components: {
+    "v-register": Register
+  },
+
+  methods: {
+    onSubmit(input) {
+      console.log("Dialog", input);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
