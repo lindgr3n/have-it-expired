6cea5a5c5ca2123db7296d5f61f54f147ea6f7cb
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-14 22:24:43 +0100

    Mediaquery to fit phone

--- src/components/molecules/Header/Header.vue
+++ src/components/molecules/Header/Header.vue
@@ -2,10 +2,10 @@
   <v-toolbar color="indigo" dark fixed app >
     <v-spacer></v-spacer>
     <v-toolbar-items v-if="user" >
-      <v-btn flat to="/"><v-icon>home</v-icon>Home</v-btn>      
-      <v-btn flat to="/register"><v-icon>event</v-icon>Register</v-btn>
-      <v-btn flat to="/about" ><v-icon>info</v-icon>About</v-btn>
-      <v-btn flat @click="onLogout"><v-icon>exit_to_app</v-icon>Logout</v-btn>
+      <v-btn flat to="/"><v-icon>home</v-icon><span class="headerText">Home</span></v-btn>      
+      <v-btn flat to="/register"><v-icon>event</v-icon><span class="headerText">Register</span></v-btn>
+      <v-btn flat to="/about" ><v-icon>info</v-icon><span class="headerText">About</span></v-btn>
+      <v-btn flat @click="onLogout"><v-icon>exit_to_app</v-icon><span class="headerText">Logout</span></v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -18,4 +18,9 @@ export default {
 </script>
 
 <style scoped>
+@media (max-width: 450px) {
+  .headerText {
+    display: none;
+  }
+}
 </style>
