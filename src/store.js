37088db6db07734d2cd6ccfcac74f54943bc1408
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-03-15 07:09:37 +0100

    Refactor to test store

--- src/store.js
+++ src/store.js
@@ -16,182 +16,184 @@ import logger from "./logger";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    items: {},
-    user: null,
-    loading: false,
-    error: null,
-    appState: null,
-    itemState: { type: null, item: null }
+const state = {
+  items: {},
+  user: null,
+  loading: false,
+  error: null,
+  appState: null,
+  itemState: { type: null, item: null }
+};
+
+export const getters = {
+  user(state) {
+    return state.user;
   },
-  getters: {
-    user(state) {
-      return state.user;
-    },
-    items(state) {
-      return state.items;
-    },
-    error(state) {
-      return state.error;
-    },
-    loading(state) {
-      return state.loading;
-    },
-    appState(state) {
-      return state.appState;
-    },
-    itemState(state) {
-      return state.itemState;
-    }
+  items(state) {
+    return state.items;
   },
-  mutations: {
-    setUser(state, user) {
-      state.user = user;
-    },
-    clearUser(state) {
-      state.user = null;
-    },
-    setItems(state, items) {
-      state.items = items;
-    },
-    addItem(state, item) {
-      state.items.push(item);
-    },
-    setLoading(state, { loading }) {
-      state.loading = loading;
-    },
-    setAppState(state, { type }) {
-      state.appState = type;
-    },
-    setError(state, message) {
-      state.error = message;
-    },
-    clearError(state) {
-      state.error = null;
-    },
-    setItemState(state, { type, item = null }) {
-      state.itemState = { type, item };
-    }
+  error(state) {
+    return state.error;
   },
-  actions: {
-    signInUser({ commit }, payload) {
-      commit("clearError");
-      // TODO: commit loading
-      const loginPromise = signInUser(payload);
-      loginPromise
-        .then(user => {
-          const newUser = Object.assign(
-            {},
-            { key: user.uid, email: user.email }
-          );
-          commit("setUser", newUser);
-          // TODO: commit success
-        })
-        .catch(error => {
-          logger.info(error.message);
-          commit("setError", error.message);
-        });
-    },
-
-    signupUser({ commit }, { email, password }) {
-      commit("clearError");
-      const createdUserPromise = signupUser({ email, password });
-      createdUserPromise
-        .then(user => {
-          // Store user to datbase
-          const newUser = Object.assign(
-            {},
-            { key: user.uid, email: user.email }
-          );
-          commit("setUser", newUser);
-          const addUserPromise = addUser(newUser);
-          addUserPromise
-            .then(() => {
-              commit("setUser", newUser);
-            })
-            .catch(error => {
-              logger.info(error.message);
-              commit("setError", error.message);
-            });
-        })
-        .catch(error => {
-          logger.info(error.message);
-          commit("setError", error.message);
-        });
-    },
-
-    signOutUser({ commit }) {
-      commit("clearError");
-      const signOutPromise = signOutUser();
-      signOutPromise
-        .then(() => {
-          commit("clearUser");
-        })
-        .catch(error => {
-          logger.info(error.message);
-          commit("setError", error.message);
-        });
-    },
-
-    addItem({ commit, state }, item) {
-      commit("clearError");
-      commit("setItemState", { type: "saving" });
-      // calculate expires
-      const expires = calculateExpires(item.bought, item.daysValid);
-      item.expires = expires;
-      const addItemPromise = addItemForUser(state.user, item);
-      addItemPromise
-        .then(data => {
-          const key = data.key;
-          const newItem = Object.assign({}, item, { id: key });
-          commit("addItem", newItem);
-          commit("setItemState", { type: "success", item: newItem });
-        })
-        .catch(error => {
-          logger.info(error.message);
-          commit("setError", error.message);
-          commit("setItemState", { type: "error" });
-        });
-    },
-
-    loadItems({ commit, state }) {
-      commit("setItemState", { type: "fetching" });
-      const loadItemsPromise = loadItemsForUser(state.user);
-      loadItemsPromise
-        .then(data => {
-          const response = data.val();
-          let items = [];
-          if (response) {
-            // Store key on item object
-            items = Object.keys(response).map(key => {
-              return Object.assign({}, response[key], { key });
-            });
-          }
-          // Object.keys(data);
-          commit("setItemState", { type: "success" });
-          commit("setItems", items);
-        })
-        .catch(error => {
-          logger.info(error.message);
-          commit("setError", error.message);
-        });
-    },
-
-    deleteItem({ commit, state, dispatch }, { key }) {
-      commit("setItemState", { type: "remove" });
-      const deleteItemsPromise = deleteItemForUser(state.user, key);
-      deleteItemsPromise
-        .then(() => {
-          commit("setLoading", { loading: false });
-          // Trigger refresh
-          dispatch("loadItems"); // TODO: make load listen on on instead of once
-          commit("setItemState", { type: "success" });
-        })
-        .catch(error => {
-          logger.info(error.message);
-          commit("setError", error.message);
-        });
-    }
+  loading(state) {
+    return state.loading;
+  },
+  appState(state) {
+    return state.appState;
+  },
+  itemState(state) {
+    return state.itemState;
   }
+};
+
+export const mutations = {
+  setUser(state, user) {
+    state.user = user;
+  },
+  clearUser(state) {
+    state.user = null;
+  },
+  setItems(state, items) {
+    state.items = items;
+  },
+  addItem(state, item) {
+    state.items.push(item);
+  },
+  setLoading(state, { loading }) {
+    state.loading = loading;
+  },
+  setAppState(state, { type }) {
+    state.appState = type;
+  },
+  setError(state, message) {
+    state.error = message;
+  },
+  clearError(state) {
+    state.error = null;
+  },
+  setItemState(state, { type, item = null }) {
+    state.itemState = { type, item };
+  }
+};
+
+export const actions = {
+  signInUser({ commit }, payload) {
+    commit("clearError");
+    // TODO: commit loading
+    const loginPromise = signInUser(payload);
+    loginPromise
+      .then(user => {
+        const newUser = Object.assign({}, { key: user.uid, email: user.email });
+        commit("setUser", newUser);
+        // TODO: commit success
+      })
+      .catch(error => {
+        logger.info(error.message);
+        commit("setError", error.message);
+      });
+  },
+
+  signupUser({ commit }, { email, password }) {
+    commit("clearError");
+    const createdUserPromise = signupUser({ email, password });
+    createdUserPromise
+      .then(user => {
+        // Store user to datbase
+        const newUser = Object.assign({}, { key: user.uid, email: user.email });
+        commit("setUser", newUser);
+        const addUserPromise = addUser(newUser);
+        addUserPromise
+          .then(() => {
+            commit("setUser", newUser);
+          })
+          .catch(error => {
+            logger.info(error.message);
+            commit("setError", error.message);
+          });
+      })
+      .catch(error => {
+        logger.info(error.message);
+        commit("setError", error.message);
+      });
+  },
+
+  signOutUser({ commit }) {
+    commit("clearError");
+    const signOutPromise = signOutUser();
+    signOutPromise
+      .then(() => {
+        commit("clearUser");
+      })
+      .catch(error => {
+        logger.info(error.message);
+        commit("setError", error.message);
+      });
+  },
+
+  addItem({ commit, state }, item) {
+    commit("clearError");
+    commit("setItemState", { type: "saving" });
+    // calculate expires
+    const expires = calculateExpires(item.bought, item.daysValid);
+    item.expires = expires;
+    const addItemPromise = addItemForUser(state.user, item);
+    addItemPromise
+      .then(data => {
+        const key = data.key;
+        const newItem = Object.assign({}, item, { id: key });
+        commit("addItem", newItem);
+        commit("setItemState", { type: "success", item: newItem });
+      })
+      .catch(error => {
+        logger.info(error.message);
+        commit("setError", error.message);
+        commit("setItemState", { type: "error" });
+      });
+  },
+
+  loadItems({ commit, state }) {
+    commit("setItemState", { type: "fetching" });
+    const loadItemsPromise = loadItemsForUser(state.user);
+    loadItemsPromise
+      .then(data => {
+        const response = data.val();
+        let items = [];
+        if (response) {
+          // Store key on item object
+          items = Object.keys(response).map(key => {
+            return Object.assign({}, response[key], { key });
+          });
+        }
+        // Object.keys(data);
+        commit("setItemState", { type: "success" });
+        commit("setItems", items);
+      })
+      .catch(error => {
+        logger.info(error.message);
+        commit("setError", error.message);
+      });
+  },
+
+  deleteItem({ commit, state, dispatch }, { key }) {
+    commit("setItemState", { type: "remove" });
+    const deleteItemsPromise = deleteItemForUser(state.user, key);
+    deleteItemsPromise
+      .then(() => {
+        commit("setLoading", { loading: false });
+        // Trigger refresh
+        dispatch("loadItems"); // TODO: make load listen on on instead of once
+        commit("setItemState", { type: "success" });
+      })
+      .catch(error => {
+        logger.info(error.message);
+        commit("setError", error.message);
+      });
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
 });
